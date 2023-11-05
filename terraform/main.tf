a7b67836825218db14e60965b755d7f050496685
SUMAN KUMAR BARIK <sukubarik@gmail.com>
2023-11-05 17:34:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,16 @@
 terraform {
-   backend "azurerm" {
-     resource_group_name  = "Terraform_1"
-     storage_account_name = "terraformnewdemo"
-     container_name       = "tfstatefile"
-     key                  = "dev.terraform.tfstate"
-   }
+  backend "azurerm" {
+    resource_group_name  = "Terraform_1"
+    storage_account_name = "terraformnewdemo"
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 module "RG" {
-  source = "./modules/RG"
-  rgname = var.rgname
+  source   = "./modules/RG" #A
+  rgname   = var.rgname     #B
   location = var.location
 }
-
 module "SA" {
   source   = "./modules/StorageAccount"
   sname    = var.sname
